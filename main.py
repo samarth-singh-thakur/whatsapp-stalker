799bbf8d4e10fe0d1daf62bfc78bd1ce421b1a45
Samarth Singh Thakur <samarth.thakur44@gmail.com>
2021-05-18 03:36:43 +0530

    removed redundancy

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
 from selenium.webdriver.common.action_chains import ActionChains
-import pyautogui
-
 stalker = webdriver.Chrome()
-import time
 from datetime import datetime
 import time
 stalker.get("https://web.whatsapp.com")
-
-input()
+sessionStart = None; 
+checked = False;
+sessions = [];
 
 while True:
-
-	try:	
-		stalker.find_element_by_xpath('//*[@title="online"]')
-		print("Aditi was online on ")
-		print(datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
-		time.sleep(5)
-		
-	except:
-		print("Aditi went offline on ")
-		print(datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
-		time.sleep(5)
-		
+	try:
+		name = stalker.find_element_by_xpath('//*[@id="main"]/header/div[2]/div/div/span').text
+		try:
+			stalker.find_element_by_xpath('//*[@title="online"]')
+			if(checked == False):
+				sessionStart = time.time();
+				checked = True
+		except:
+			if(sessionStart!=None):
+				sessionEnd = datetime.utcfromtimestamp(time.time()).strftime('%H:%M:%S')
+				duration = int(time.time() - sessionStart)
+				sessioStart = datetime.utcfromtimestamp(sessionStart).strftime('%H:%M:%S')
+				# print(sessioStart, str(duration), sessionEnd)
+				sessions.append([sessioStart, str(duration), sessionEnd])
+				if(checked == True):			
+					print("{}. {} opened whatsapp at {}, stayed for {} seconds and went offline on {}".format(len(sessions),name,sessions[-1][0],sessions[-1][1],sessions[-1][2]))
+					checked = False;
+				sessionStart = None
+	except Exception as e:
+		print(e,end = "\r")
+		pass
 
 		
 
